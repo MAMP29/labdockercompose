88679a58df0e0e559af20790758edd07e322e1fb
jachuone <james.calero@correounivalle.edu.co>
2025-04-05 00:33:53 -0500

    modificacion del puerto

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { name: "Service 1", url: "http://localhost:5001/api" },
         { name: "Service 2", url: "http://localhost:5002/api" },
         { name: "Service 3", url: "http://localhost:5003/api" },
+        { name: "Service 5", url: "http://localhost:5005/api" },
         { name: "Service 6", url: "http://localhost:5006/api" },
         { name: "Service 7", url: "http://localhost:5007/api" },
         { name: "Service 8", url: "http://localhost:5008/api" },
